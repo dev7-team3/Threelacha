5a99a97b9f2c3f5a2dce475ddedd8baddae41eb5
dah83 <aldhss26@gmail.com>
2025-12-31 11:26:00 +0900

    fix: change stg file (#16)

--- dbt/Threelacha/models/gold/stg_daily_product_price.sql
+++ dbt/Threelacha/models/gold/stg_daily_product_price.sql
@@ -1,5 +1,12 @@
 {{ config(materialized='ephemeral') }}
 
+WITH ranked_data AS (
+    SELECT 
+        *,
+        -- dt 기준 내림차순
+        RANK() OVER (ORDER BY dt DESC) as rnk
+    FROM {{ source('silver', 'api10') }}
+)
 SELECT
     -- 산물 정보
     product_no,
@@ -29,8 +36,5 @@ SELECT
     prev_1y_pr,
 
     dt
-FROM {{ source('silver', 'api10') }}
-WHERE dt = (
-    SELECT MAX(dt)
-    FROM {{ source('silver', 'api10') }}
-)
+FROM ranked_data
+WHERE rnk = 1
